780bf7911042548ef8626678515d5f99863a426c
Kirill Fedotov <svotee@mail.ru>
2022-01-20 09:32:52 +0300

    first upload

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict'
+function one(x){
+    function two(){
+    
+    
+    const a = prompt('Введите число')
+    if (typeof(a)=='string'){
+    if (Number(a) !== x){
+        if(a>x){
+            alert('Ваше число больше загаданного')
+        }
+        if(a<x){
+            alert('Ваше число меньше загаданного')
+        }
+        two()
+    }
+    if (Number(a) == x){
+        alert('Вы угадали число')
+    }
+    }
+    if(typeof(a)!='string'){
+        alert('Введите именно число')
+        two()
+    }
+}
+    two()
+}
+one((Math.floor(Math.random() * 100) + 1))
